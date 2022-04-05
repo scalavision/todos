bf54fd0ec6d23ee45dc87be1be06b1ba3cbae563
scalavision <scalavision@gmail.com>
2022-04-05 20:56:35 +0200

    [todo app] initial running code

--- ui/src/main/scala/todos/Main.scala
+++ ui/src/main/scala/todos/Main.scala
@@ -1,6 +1,11 @@
 package todos
 
+import com.raquo.laminar.api.L
 object Main:
 
   def main(args: Array[String]): Unit =
-    println("hello world")
+    todos.dom.init()
+    todos.dom.renderOnWindowContentLoaded(
+      todos.dom.app(),
+      todos.Page()
+    )
---
+++ ui/src/main/scala/todos/page.scala
@@ -0,0 +1,8 @@
+package todos
+
+import com.raquo.laminar.api.L
+import com.raquo.laminar.api.L.*
+
+def Page() = L.div(
+  "Hello World"
+)
